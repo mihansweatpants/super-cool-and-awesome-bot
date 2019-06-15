e4a3ebbf6a2a09486e643826bc7b004a3eb3ba96
mihansweatpants <mihansweatpants@gmail.com>
2019-06-15 22:02:34 +0300

    Add response for empty schedule

--- src/modules/timetable/format.ts
+++ src/modules/timetable/format.ts
@@ -6,17 +6,22 @@ interface FormatParams {
   week: string;
 }
 
+const emptyDayResponse = 'Пар нет 😎';
+
 // tslint:disable prefer-template
 export function formatTimetableResponse({ day, classes, week }: FormatParams) {
-
   let formatted = `${'='.repeat(5)} ${day} ${'='.repeat(5)}\n\n`;
 
   if (week) {
     formatted = `${'*'.repeat(10)} ${week} ${'*'.repeat(10)}\n` + formatted;
   }
 
-  for (const { time, subject } of classes) {
-    formatted += `\n${time}\n` + `${subject}\n${'_'.repeat(20)}\n`;
+  if (classes.length > 0) {
+    for (const { time, subject } of classes) {
+      formatted += `\n${time}\n` + `${subject}\n${'_'.repeat(20)}\n`;
+    }
+  } else {
+    formatted += `${' '.repeat(15)} ${emptyDayResponse} ${' '.repeat(15)}\n`;
   }
 
   return formatted;
